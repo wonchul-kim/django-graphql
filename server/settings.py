fd7152ad5409fb217f581213f4a67af6aad3b2fa
wonchul <onedang22@gmail.com>
2023-05-26 10:23:10 +0900

    change sqlite to postgresql

--- server/settings.py
+++ server/settings.py
@@ -82,11 +82,22 @@ WSGI_APPLICATION = 'server.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'athena',
+        'USER': 'aiv',
+        'PASSWORD': 'aiv11011',
+        'HOST': '192.168.10.41',
+        'PORT': 5432, 
+    }  
 }
 
 
